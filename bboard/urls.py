c7380449b7069cfb64181be11875f8fe4b081cb6
evgeniy_liubich <lyubitch89@yandex.by>
2020-05-28 14:54:56 +0300

    Обратное разрешение интернет-адресов

--- bboard/urls.py
+++ bboard/urls.py
@@ -4,6 +4,6 @@ from . import views
 from .views import by_rubric
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', views.index, name='index'),
 ]
