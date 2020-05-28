74f836857c99c46185018571916c20c6c5030b0f
evgeniy_liubich <lyubitch89@yandex.by>
2020-05-28 12:43:04 +0300

    Template by_rubric.html

--- bboard/models.py
+++ bboard/models.py
@@ -17,6 +17,8 @@ class Bb(models.Model):  # Модель объявления
 
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name_plural = 'Рубрики'
--- bboard/urls.py
+++ bboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import by_rubric
 
-urlpatterns = [
+urlpatterns = {
+    path('<int:rubric_id>', by_rubric),
     path('', views.index, name='index'),
-]
+}
