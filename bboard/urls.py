d957b8b27c14fc16150f7c56d1ee98ba7dc953bc
evgeniy_liubich <lyubitch89@yandex.by>
2020-06-05 10:35:56 +0300

    Код шаблона create.html для контроллер-класса

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from . import views
-from .views import by_rubric
+from .views import by_rubric, BbCreateView
 
 urlpatterns = [
+    path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', views.index, name='index'),
 ]
